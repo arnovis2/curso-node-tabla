aa7e88206e0e4e6f7d7e34b6bd169ae379b0c928
Arnovis Mendoza <arnovis_2@hotmail.com>
2022-08-19 16:56:35 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const { argv } = require('process');
+const {crearArchivo}=require('./helpers/multiplicar')
+console.clear();
+
+
+console.log(process.argv);
+console.log(argv);
+console.log('base: yargs',argv.base);
+
+// const base =2;
+
+// crearArchivo(base)
+// .then(nombreArchivo =>console.log (nombreArchivo, 'creado'))
+// .catch(err => console.log (err));
+    
